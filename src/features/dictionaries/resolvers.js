846ba7483f7e8608722e63010e39a85731cb17b4
karenalo13 <andrei.diaconu013@yahoo.ro>
2021-10-04 10:05:00 +0300

    vineri

--- src/features/dictionaries/resolvers.js
+++ src/features/dictionaries/resolvers.js
@@ -1,2 +1,26 @@
-const dictionaryResolvers = {}
+const dictionaryResolvers = {
+  Query: {
+    typeList: async (_parent, _args, { dataSources }, _info) => {
+      const typeList = await dataSources.dictionaryDb.getTypeList()
+      return typeList
+    },
+    categoryList: async (_parent, _args, { dataSources }, _info) => {
+      const categoryList = await dataSources.dictionaryDb.getCategoryList()
+      return categoryList
+    },
+    countryList: async (_parent, _args, { dataSources }, _info) => {
+      const categoryList = await dataSources.dictionaryDb.getCountryList()
+      return categoryList
+    },
+    countyList: async (_parent, _args, { dataSources }, _info) => {
+      const categoryList = await dataSources.dictionaryDb.getCountyList()
+      return categoryList
+    },
+    cityList: async (_parent, _args, { dataSources }, _info) => {
+      const categoryList = await dataSources.dictionaryDb.getCityList()
+      return categoryList
+    }
+  }
+}
+
 module.exports = dictionaryResolvers
--- src/startup/dataSources.js
+++ src/startup/dataSources.js
@@ -1,10 +1,12 @@
 const ConferenceDb = require('../features/conference/conferenceDb')
 const UserApi = require('../features/user/dataSources/userApi')
 const UserDb = require('../features/user/dataSources/userDb')
+const DictionaryDb = require('../features/dictionaries/dictionaryDb')
 const ds = {
   userApi: new UserApi(),
   userDb: new UserDb(),
-  conferenceDb: new ConferenceDb()
+  conferenceDb: new ConferenceDb(),
+  dictionaryDb:new DictionaryDb()
 }
 module.exports.getDataSources = () => ds
 // This is a temporary fix to pass dataSources to ws requests. This will be fixed in Apollo server v3.0
